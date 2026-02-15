6b577ab55df3224a7830600154fa2651ba57e5a8
harshithahanur646@gmail.com <harshithahanur646@gmail.com>
2026-02-15 19:38:10 +0000

    committing to dev from fb_models

---
+++ models/staging/customers.sql
@@ -0,0 +1,12 @@
+
+{{ config(materialized=
+
+'view') }}
+WITH tb1 as(
+    select
+    id ,
+    first_name,
+    last_name
+    from {{source('datafeed_shared_schema','raw_customerdata')}})
+select * from tb1
+
